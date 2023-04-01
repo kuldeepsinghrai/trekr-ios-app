d9dd6b176e6bd15cb2f973bff9e10ee9f0d58741
KuldeepSinghRai <imkuldeepsinghrai@gmail.com>
2023-04-01 11:38:56 +0530

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Kuldeep Singh Rai on 01/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
